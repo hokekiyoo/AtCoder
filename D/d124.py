ee63214597128873a4037c272af9930bb6f9dbc1
hokekiyoo <imslotter1@gmail.com>
2019-04-15 17:59:50 +0900

    実装難しい

--- D/d124.py
+++ D/d124.py
@@ -1,8 +1,10 @@
+from itertools import accumulate
+
 N, K = map(int,input().split())
 S = input()
+
 nums = []
-tmp = S[0]
-init = int(tmp)
+tmp = "1" #はじめに0が入ったら、0からスタートというのがわかる。
 count = 0
 for s in S:
     if tmp == s:
@@ -11,24 +13,23 @@ for s in S:
         nums.append(count)
         count = 1
         tmp = s
+nums.append(count)
+if tmp == "0":
+    nums.append(0) #エッジの条件
 
-if init == 0:
-    #始め0の場合
-    lenmax = sum(nums[:2*K])
-    #それ以外は1ことばし
-    for i in range(1,len(nums)-2*K,2):
-        if i + 2*K == len(nums):
-            lenmax = max(lenmax,sum(nums[i:i+2*K]))
-        else:
-            lenmax = max(lenmax,sum(nums[i:i+2*K+1]))
+# print(nums)
+# 累積和表示にする(listって足し算できるのね)
+# これによって、各文字最初の値が明らかになる
+acc = [0]+list(accumulate(nums))
+# print(acc)
+n = len(nums)
+w = 2*K + 1
 
-elif init == 1:
-    lenmax = 0
-    #それ以外は1ことばし
-    for i in range(len(nums)-2*K,2):
-        if i + 2*K == len(nums):
-            lenmax = max(lenmax,sum(nums[i:i+2*K]))
-        else:
-            lenmax = max(lenmax,sum(nums[i:i+2*K+1]))
+if len(nums) <= w:
+    print(N)
+else:
+    ans = 0
+    for i in range(0,n-w+1,2): #2個飛ばし
+        ans = max(ans,acc[i+w]-acc[i])
 
-print(lenmax)
+    print(ans)
