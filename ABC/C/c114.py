265d69fca02f2953e16088add9c960f3b836204d
hokekiyoo <imslotter1@gmail.com>
2019-05-09 01:20:39 +0900

    dfsをstackで

--- ABC/C/c114.py
+++ ABC/C/c114.py
@@ -1,25 +1,3 @@
-# N = int(input())
-# import numpy as np
-
-# def num(M):
-#     return 3**M - 3*2**M + 3*1**M
-
-# allnum = 0
-# M = int(np.log10(N)) #桁数
-# X = N // 10**M #最上位桁数
-
-# keta = [3,7,5]
-# strN = str(N)
-# for i,c in enumerate(strN):
-#     m = M-i
-#     if int(c) == 3:
-#         allnum += 3**(m-1)-3*2**(m-1)+3*1
-#     if int(c) == 5:
-#         allnum += 3**(m)-2*2**(m)+1
-#     if int(c) == 7:
-#         allnum += 2*(3**(m)-2*2**(m)+1)
-#     else: break
-# print(allnum)
 
 N = int(input())
 def dfs(s): # 文字列 s で始まる七五三数の個数
@@ -33,3 +11,19 @@ def dfs(s): # 文字列 s で始まる七五三数の個数
     return ret
 
 print(dfs('0')) # 本当は dfs('') と書きたいが 4 行目でのエラーを防ぐため仕方なく
+
+# 2019/5/8 とき直し(stack)
+from collections import deque
+N = int(input())
+stack = deque(["0"])
+cnt = 0
+while len(stack)>0:
+    c = stack.pop()
+    for s in "357":
+        cs = c+s
+        if int(cs)<=N:
+            stack.append(cs)
+            if cs.count("3")*cs.count("5")*cs.count("7")>0:
+                cnt += 1
+print(cnt)
+        
