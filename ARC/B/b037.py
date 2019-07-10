82e7223a3dfff5db971fbe7c60172387c8c70a7c
hokekiyoo <imslotter1@gmail.com>
2019-07-10 18:04:01 +0900

    グラフ問題練習

---
+++ ARC/B/b037.py
@@ -0,0 +1,36 @@
+
+from collections import deque
+
+# 木構造を作る
+N, M = map(int, input().split())
+G = [[] for i in range(N)]
+
+for i in range(M):
+    u,v = map(int,input().split())
+    G[u-1].append((v-1))
+    G[v-1].append((u-1))
+
+used = [0] * N
+def dfs(G, v,pre_v):
+    global flg
+    # vを訪問済みにする
+    used[v] += 1
+    #グラフ中の次のVについて
+    for next_v in G[v]:
+        # 無向グラフ分.しゃーなし
+        if pre_v == next_v:
+            continue
+        # それ以外で地雷踏む
+        elif used[next_v]:
+            flg = False
+        # 見られていない場合、再帰的に探索
+        else:
+            dfs(G, next_v,v)
+
+cnt = 0
+for i in range(N):
+    if not used[i]:
+        flg = True
+        dfs(G,i,-1)
+        cnt += flg 
+print(cnt)
